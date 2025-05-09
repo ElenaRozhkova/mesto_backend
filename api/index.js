27fd115df707d5c2cc58e6d15ec8eebea77912fc
Elena Rozhkova <elenaroschkowa@gmx.de>
2025-05-09 11:46:34 +0200

    test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -5,19 +6,13 @@ const bodyParser = require('body-parser');
 const { requestLogger, errorLogger } = require('../middlewares/logger');
 const routeUser = require('../routes/users');
 const routeCard = require('../routes/cards');
-const { login, createUser } = require('../controllers/users');
 const NotFoundError = require('../errors/not-found.js');
 const centralerrors = require('../middlewares/central-errors.js');
 const { errors } = require('celebrate');
 require('dotenv').config();
 
-const serverless = require('serverless-http');
-
-// Создаем экземпляр приложения Express
 const app = express();
-const { PORT = 3002 } = process.env;
 
-// Подключаемся к MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -41,4 +36,4 @@ app.get('*', () => {
 });
 
 // Экспортируем сервер как серверлес-функцию для Vercel
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
--- app.js
+++ app.js
@@ -1,44 +1,72 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { requestLogger, errorLogger } = require('../middlewares/logger');
-const routeUser = require('../routes/users');
-const routeCard = require('../routes/cards');
-const { login, createUser } = require('../controllers/users');
-const NotFoundError = require('../errors/not-found.js');
-const centralerrors = require('../middlewares/central-errors.js');
-const { errors } = require('celebrate');
-require('dotenv').config();
 
-const serverless = require('serverless-http');
+const { celebrate, Joi, errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const routeUser = require('./routes/users');
+const routeCard = require('./routes/cards');
+const { login, createUser } = require('./controllers/users');
+const NotFoundError = require('./errors/not-found.js');
+const centralerrors = require('./middlewares/central-errors.js');
+//const auth = require('./middlewares/auth');
 
-// Создаем экземпляр приложения Express
-const app = express();
 const { PORT = 3002 } = process.env;
 
-// Подключаемся к MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB подключено!'))
-  .catch((err) => console.error('Ошибка подключения MongoDB:', err));
-
+const app = express();
 app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
-app.use(requestLogger);
-app.use(routeUser);
-app.use(routeCard);
-app.use(errorLogger);
-app.use(errors());
-app.use(centralerrors);
-
-app.get('*', () => {
-  throw new NotFoundError('Ресурс не найден');
 });
 
-// Экспортируем сервер как серверлес-функцию для Vercel
-module.exports.handler = serverless(app);
+/*mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(requestLogger); // подключаем логгер запросов*/
+console.log("hallo");
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+/*
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    password: Joi.string().required(),
+    email: Joi.string().required().email(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    password: Joi.string().required(),
+    email: Joi.string().required().email(),
+  }),
+}), createUser);
+
+app.use(auth, routeUser);*/
+
+//app.use(auth, routeCard);
+
+app.use(routeUser);
+
+app.use(routeCard);
+
+app.use(errorLogger); // подключаем логгер ошибок
+app.get('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не существует');
+});
+
+// обработчики ошибок
+app.use(errors()); // обработчик ошибок celebrate
+
+app.use(centralerrors);
+app.get('/', (req, res) => {
+  res.send('Сервер работает!');
+});
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
